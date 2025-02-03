32ebc9e92a5e5afc249f479df588d9e287916429
PrimakMV <pm2710@mail.ru>
2025-02-03 19:26:17 +0300

    Implemented a controller for processing a task creation request

---
+++ src/main/java/com/fluxtasker/controller/TaskController.java
@@ -0,0 +1,33 @@
+package com.fluxtasker.controller;
+
+import com.fluxtasker.bom.response.ApiResponse;
+import com.fluxtasker.dto.TaskCreateDTO;
+import com.fluxtasker.exception.TaskCreationException;
+import com.fluxtasker.model.Task;
+import com.fluxtasker.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    public Mono<ResponseEntity<ApiResponse<Task>>> createTask(@RequestBody TaskCreateDTO taskDTO) {
+        return taskService.createTask(taskDTO)
+                .map(task -> ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(task)))
+                .onErrorResume(TaskCreationException.class, ex ->
+                        Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                .body(new ApiResponse<>(ex.getMessage())))
+                );
+    }
+}
