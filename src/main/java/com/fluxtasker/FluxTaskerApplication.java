a7eec0e7f63a5b0cab7ebe63640510604823d289
PrimakMV <pm2710@mail.ru>
2025-02-03 16:54:57 +0300

    Added the structure of the test project

---
+++ src/main/java/com/fluxtasker/FluxTaskerApplication.java
@@ -0,0 +1,13 @@
+package com.fluxtasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxTaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxTaskerApplication.class, args);
+	}
+
+}
