35e9139aa4246fc739d78b72ab398afeddde19c6
PrimakMV <pm2710@mail.ru>
2025-02-04 14:11:18 +0300

    Added error handling when creating a comment

--- src/main/java/com/fluxtasker/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/fluxtasker/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.fluxtasker.exception.handler;
 
 import com.fluxtasker.bom.ErrorResponse;
+import com.fluxtasker.exception.CommentCreationException;
 import com.fluxtasker.exception.TaskCreationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(TaskCreationException.class)
     public ResponseEntity<ErrorResponse> handlerTaskCreationException(TaskCreationException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(CommentCreationException.class)
+    public ResponseEntity<ErrorResponse> handlerCommentCreationException(CommentCreationException ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(ex.getMessage()));
     }
 }
