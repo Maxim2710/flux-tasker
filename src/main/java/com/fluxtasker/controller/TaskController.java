88555b8499c80e7a557666385f4e12e6ca2ef05a
PrimakMV <pm2710@mail.ru>
2025-02-04 12:11:50 +0300

    Added a request handler for listening to task creation and update events

--- src/main/java/com/fluxtasker/controller/TaskController.java
+++ src/main/java/com/fluxtasker/controller/TaskController.java
@@ -7,6 +7,7 @@ import com.fluxtasker.model.Task;
 import com.fluxtasker.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -64,5 +65,10 @@ public class TaskController {
                         return Mono.just(ResponseEntity.notFound().build());
                     }
                 });
+    }
+
+    @GetMapping(path = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Task> streamTasks() {
+        return taskService.streamTasks();
     }
 }
