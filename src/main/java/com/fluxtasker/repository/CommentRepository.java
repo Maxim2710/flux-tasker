c1cc4ac8d6e9e22411d3b78a238b950c6f9cf21f
PrimakMV <pm2710@mail.ru>
2025-02-04 14:34:49 +0300

    Implemented business logic to receive all comments on a particular task

--- src/main/java/com/fluxtasker/repository/CommentRepository.java
+++ src/main/java/com/fluxtasker/repository/CommentRepository.java
@@ -3,8 +3,10 @@ package com.fluxtasker.repository;
 import com.fluxtasker.model.Comment;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 @EnableR2dbcRepositories
 public interface CommentRepository extends ReactiveCrudRepository<Comment, Long> {
 
+    Flux<Comment> findByTaskId(Long taskId);
 }
--- src/main/java/com/fluxtasker/service/CommentService.java
+++ src/main/java/com/fluxtasker/service/CommentService.java
@@ -5,6 +5,7 @@ import com.fluxtasker.model.Comment;
 import com.fluxtasker.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -23,5 +24,9 @@ public class CommentService {
 
         return commentRepository.save(comment)
                 .onErrorMap(error -> new CommentCreationException("Ошибка при создании комментария"));
+    }
+
+    public Flux<Comment> getCommentsByTaskId(Long taskId) {
+        return commentRepository.findByTaskId(taskId);
     }
 }
